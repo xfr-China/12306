05a6a54d90a62df8d995a79585c174a21dba1046
Lumine <2068315650@qq.com>
2026-01-25 21:50:04 +0800

    清理项目中的 Apache License 和公众号推广注释

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/job/TrainStationJobHandler.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/job/TrainStationJobHandler.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.opengoofy.index12306.biz.ticketservice.job;
 
 import com.alibaba.fastjson2.JSON;
@@ -39,7 +22,6 @@ import static org.opengoofy.index12306.biz.ticketservice.common.constant.RedisKe
 /**
  * 列车路线信息定时任务
  * 已通过运行时判断缓存不存在实时读取数据库获取完成，该定时任务不在主流程中
- * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
  */
 @Deprecated
 @RestController
